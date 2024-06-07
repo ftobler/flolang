1a7ac07a44b8f5f6c935b7168c438f1c68f151e0
Florin Tobler <florin.tobler@hotmail.com>
2024-06-07 15:29:44 +0200

    better expressions

--- tests/test_algorythm.py
+++ tests/test_algorythm.py
@@ -62,10 +62,12 @@ fn crc8_update(int data, int polynomial=0x07) int:
 
 const int init_value = 0x00
 let int crc = init_value
-crc = crc8_update(1)
-crc = crc8_update(2)
-crc = crc8_update(3)
-crc = crc8_update(4)
+for int i in 1..4:
+    crc = crc8_update(i)
+
+# print result
+print(crc)
+crc
 
 """) == crc8([1,2,3,4])
 
@@ -87,10 +89,9 @@ fn crc8_update(int data, int polynomial=0x07) int:
 const int init_value = 0x00
 const int polynomial = 0x10
 let int crc = init_value
-crc = crc8_update(1, polynomial)
-crc = crc8_update(2, polynomial)
-crc = crc8_update(3, polynomial)
-crc = crc8_update(4, polynomial)
+for int i in 1..4:
+    crc = crc8_update(i, polynomial)
+crc
 
 """) == crc8([1,2,3,4], 0x10)
 
@@ -112,7 +113,8 @@ let int fac
 
 # check if the number is negative
 if num < 0:
-    fac = "Sorry, factorial does not exist for negative numbers"
+    print("Sorry, factorial does not exist for negative numbers")
+    fac = -1
 elif num == 0:
     fac = 1
 else:
