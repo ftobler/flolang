a851f1bc798f8e54295e83914f01243a87251425
Florin Tobler <florin.tobler@hotmail.com>
2024-06-08 14:33:46 +0200

    checker that readme examples are always up to date with scripts.

---
+++ tests/test_readme_examples.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tests.context import resolve_path
+from flolang import tokenize, default_environment, parse, interpret
+import pytest
+
+
+def read_file(file_path):
+    with open(resolve_path(file_path)) as f:
+        return f.read()
+
+def check_file_is_containted_in_file(child, container):
+    assert read_file(container).index(read_file(child)) >= 0 # means file is contained in README.md
+
+def test_script_is_in_readme_1():
+    check_file_is_containted_in_file("./code/test_code_readme_example_1.txt", "../README.md")
+
+def test_script_is_in_readme_2():
+    check_file_is_containted_in_file("./code/test_code_readme_example_2.txt", "../README.md")
