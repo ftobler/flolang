77473114bc44a6f024f969b41c625d32fb6ad13a
Florin Tobler <florin.tobler@hotmail.com>
2025-07-22 18:08:22 +0200

    switch to temp directory

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,16 +1,23 @@
 import pytest
 from flolang.compiler import compiler_run
 import os
-import time
 
 
-def test_compile_to_ast():
-    t = time.time()
-    compiler_run(["floc", "./tests/code/test_code.txt", "--emit", "ast"])
-    assert os.path.getmtime("./tests/code/test_code.ast") > t
+def test_compile_to_ast(tmp_path):
+    output_path = tmp_path / "file.ast"
+    compiler_run(["floc", "./tests/code/test_code.txt", "--emit", "ast", "--output", output_path])
+    assert os.path.isfile(output_path)
+    with open(output_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        [print(s) for s in lines]
+        assert len(lines) > 10
 
 
-def test_compile_to_token():
-    t = time.time()
-    compiler_run(["floc", "./tests/code/test_code.txt", "--emit", "token"])
-    assert os.path.getmtime("./tests/code/test_code.token") > t
+def test_compile_to_token(tmp_path):
+    output_path = tmp_path / "file.token"
+    compiler_run(["floc", "./tests/code/test_code.txt", "--emit", "token", "--output", output_path])
+    assert os.path.isfile(output_path)
+    with open(output_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        [print(s) for s in lines]
+        assert len(lines) > 10
