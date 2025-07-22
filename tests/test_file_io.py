633dde6c114a24cc916a958c0995b4362f554cd8
Florin Tobler <florin.tobler@hotmail.com>
2025-07-22 17:56:40 +0200

    fix syntax, fixture

--- tests/test_file_io.py
+++ tests/test_file_io.py
@@ -1,13 +1,13 @@
 import pytest
 
 
-def test_file_print_character1(temp_path):
-    file_path = temp_path // "file.txt"
+def test_file_print_character1(tmp_path):
+    file_path = tmp_path / "file.txt"
     with open(file_path, "w", encoding="utf-8") as f:
         print("\u251c", file=f)
 
 
-def test_file_print_character2(temp_path):
-    file_path = temp_path // "file.txt"
+def test_file_print_character2(tmp_path):
+    file_path = tmp_path / "file.txt"
     with open(file_path, "w", encoding="utf-8") as f:
         print("├", file=f)
